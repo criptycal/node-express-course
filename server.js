7dab93407879d17373b4eabc5e4aec60a1802a48
laEsquinaLlanera <laesquinallanera333@gmail.com>
2021-08-02 19:23:02 -0500

    add dynamic GET route

--- server.js
+++ server.js
@@ -10,15 +10,23 @@ const mockUserData = [
     }
 ]
 
-app.get('./users', (req, res) =>{
+
+
+app.get('/users', (req, res) =>
     res.json({
         success: true,
         message: 'Successfully got users. NICE!',
         users: mockUserData
-    })
-})
+    }))
 
-app.listen(8000, () =>{
-    console.log("server is listening bro!")
+app.listen(8000, () => console.log("server is listening bro!"))
+
+app.get('/users/:id', (req, res)  =>{
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'got one user bro!',
+        user: req.params.id
+    })
 })
 
