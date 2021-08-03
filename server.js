01d6ce8297a1b4bb65b4b67aa18a29d0fcd6d0fa
laEsquinaLlanera <laesquinallanera333@gmail.com>
2021-08-02 19:11:29 -0500

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require ('express');
 const app = express();
 
-app.listen(8000, () =>{
-    console.log("server is running bro!")
+const mockUserData = [
+    {
+        name: 'David'
+    },
+    {
+        name: 'Victoria'
+    }
+]
+
+app.get('./users', (req, res) =>{
+    res.json({
+        success: true,
+        message: 'Successfully got users. NICE!',
+        users: mockUserData
+    })
 })
+
+app.listen(8000, () =>{
+    console.log("server is listening bro!")
+})
+
